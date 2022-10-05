6639523654c6eb72763b3ac1cecca80ace55079b
trifa <trifa@connect.hku.hk>
2022-10-05 17:27:21 +0800

    add the initial UI of new question page and handle the button disable condition

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,14 +1,61 @@
-import React from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
+import { Button, Form } from "react-bootstrap";
 
 const NewPoll = (props) => {
-  return (
-    <div>This is the New Poll Page.</div>
-  );
+    const [optionOneText, setOptionOneText] = useState("");
+    const [optionTwoText, setOptionTwoText] = useState("");
+
+    const isButtonDisable = () => optionOneText === "" || optionTwoText === ""; 
+
+    const handleChange = (e, setText) => {
+        const text = e.target.value;
+        setText(text);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(
+            `new question with option one ${optionOneText}, with option two ${optionTwoText}`
+        );
+        setOptionOneText("");
+        setOptionTwoText("");
+    };
+
+    return (
+        <div>
+            <h1>Would You Rather</h1>
+            <h2>Create Your Own Poll</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>First Option</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Option One"
+                        value={optionOneText}
+                        onChange={(e) => handleChange(e, setOptionOneText)}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Second Option</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Option Two"
+                        value={optionTwoText}
+                        onChange={(e) => handleChange(e, setOptionTwoText)}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit" disabled={isButtonDisable()}>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
 };
 
 const mapStateToProps = ({ authedUser, users, questions }) => {
-
+  return {};
 };
 
 export default connect(mapStateToProps)(NewPoll);
