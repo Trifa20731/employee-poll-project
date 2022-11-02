9ca51212f1e38d9e2671eb2fd91c4a1c5ee6bf5a
Kai Sum Cheung <trifa@KaideMacBook-Pro.local>
2022-11-02 17:37:33 +0800

    add the _saveQuestionAnswer test

--- src/test/_DATA.test.js
+++ src/test/_DATA.test.js
@@ -1,4 +1,4 @@
-import { _saveQuestion } from '../utils/_DATA';
+import { _saveQuestion, _saveQuestionAnswer } from '../utils/_DATA';
 
 describe("_saveQuestion", () => {
   it("will return format question if options and author are not null", async () => {
@@ -21,5 +21,27 @@ describe("_saveQuestion", () => {
     await expect(_saveQuestion(unavailableQuestion)).rejects.toEqual(
       "Please provide optionOneText, optionTwoText, and author"
     );
+  });
+});
+
+describe("_saveQuestionAnswer", () => {
+  it("will return true of authedUser, qid and answer are not undefined", async () => {
+    const availableQuestionAnswer = {
+      authedUser: "tylermcginnis",
+      qid: "am8ehyc8byjqgar0jgpub9",
+      answer: "optionOne"
+    }
+    var result = await _saveQuestionAnswer(availableQuestionAnswer);
+    expect(result).toEqual(true);
+  });
+
+  it("will return an error message if answer is undefined.", async () => {
+    const unavailableQuestionAnswer = {
+      authedUser: "tylermcginnis",
+      qid: "am8ehyc8byjqgar0jgpub9",
+    }
+    await expect(_saveQuestionAnswer(unavailableQuestionAnswer)).rejects.toEqual(
+      "Please provide authedUser, qid, and answer"
+    );    
   });
 });
