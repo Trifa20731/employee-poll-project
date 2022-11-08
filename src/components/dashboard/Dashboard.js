46a4c5c0f25d22ef9fc91cadd9ff938b1a20e409
trifa <trifa@connect.hku.hk>
2022-11-08 17:56:34 +0800

    add the radio button at the dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,36 +1,68 @@
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import QuestionItem from "./QuestionItem";
-import { Row } from "react-bootstrap";
-import "../../css/Dashboard.css"
+import { Row, ButtonGroup, ToggleButton } from "react-bootstrap";
+import "../../css/Dashboard.css";
 
 const Dashboard = (props) => {
   // console.log(props.newQuestionIds);
   // console.log(props.doneQuestionIds);
+  const [radioValue, setRadioValue] = useState("0");
+
+  const radios = [
+    { name: "New Question", value: "0" },
+    { name: "Done", value: "1" },
+    { name: "Both", value: "2" },
+  ];
+
   return (
     <div className="container">
-      <h1 className="title">New Question</h1>
-        <Row xs={1} md={4}>
-          {
-            props.newQuestionIds.map((id) => (
-              <QuestionItem key={id} id={id}/>
-            ))
-          }
-        </Row>
-      <h1 className="title">Done</h1>
-        <Row xs={1} md={4}>
-          {
-            props.doneQuestionIds.map((id) => (
-              <QuestionItem key={id} id={id}/>
-            ))
-          }
-        </Row>
+      <ButtonGroup>
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant="outline-primary"
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+      {
+        radioValue === "1" ? (<br />) : (
+          <div>
+            <h1 className="title">New Question</h1>
+            <Row xs={1} md={4}>
+              {props.newQuestionIds.map((id) => (
+                <QuestionItem key={id} id={id} />
+              ))}
+            </Row>
+          </div>
+        )
+      }
+      {
+        radioValue === "0" ? (<br />) : (
+          <div>
+            <h1 className="title">Done</h1>
+            <Row xs={1} md={4}>
+              {props.doneQuestionIds.map((id) => (
+                <QuestionItem key={id} id={id} />
+              ))}
+            </Row>
+          </div>
+        )
+      }
     </div>
   );
 };
 
 // TODO: Change the state conversion.
 const mapStateToProps = ({ authedUser, questions }) => {
-
   // Get the done questions and new question.
   const newQuestions = [];
   const doneQuestions = [];
@@ -38,30 +70,32 @@ const mapStateToProps = ({ authedUser, questions }) => {
     const question = questions[questionId];
     const votesOptionOne = question.optionOne.votes;
     const votesOptionTwo = question.optionTwo.votes;
-    if (votesOptionOne.includes(authedUser.id) || votesOptionTwo.includes(authedUser.id)) {
+    if (
+      votesOptionOne.includes(authedUser.id) ||
+      votesOptionTwo.includes(authedUser.id)
+    ) {
       doneQuestions.push(question);
     } else {
       newQuestions.push(question);
     }
-  };
+  }
 
   // sort the question
   const newQuestionSorted = newQuestions.sort(
     (a, b) => b.timestamp - a.timestamp
-  )
+  );
   const doneQuestionSorted = doneQuestions.sort(
     (a, b) => b.timestamp - a.timestamp
-  )
+  );
 
   // get question id.
-  const newQuestionIds = newQuestionSorted.map(question => question.id)
-  const doneQuestionIds = doneQuestionSorted.map(question => question.id)
+  const newQuestionIds = newQuestionSorted.map((question) => question.id);
+  const doneQuestionIds = doneQuestionSorted.map((question) => question.id);
 
   return {
     newQuestionIds,
-    doneQuestionIds
+    doneQuestionIds,
   };
-
 };
 
 export default connect(mapStateToProps)(Dashboard);
